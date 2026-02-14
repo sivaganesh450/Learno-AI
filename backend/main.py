d4017a4809e001e3436349d64521b6e9e849143e
Sivaganesh450 <sivapalla777@gmail.com>
2026-02-14 14:24:25 +0530

    fixed render errors

--- backend/main.py
+++ backend/main.py
@@ -21,9 +21,21 @@ app = FastAPI(
 )
 
 # Configure CORS
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+]
+
+# Add production origins from env
+import os
+env_origins = os.environ.get("CORS_ORIGINS")
+if env_origins:
+    origins.extend(env_origins.split(","))
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
