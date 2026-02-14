581f6be0f00ef3b41829c336e68175c39f62b55f
Sivaganesh450 <sivapalla777@gmail.com>
2026-02-14 19:01:45 +0530

    chagned origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,6 +35,7 @@ if env_origins:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
